e7b5759fe0d48390bc6f9c10a0030dd3d8b19fff
Pooja <pooja.kaul2015@gmail.com>
2018-08-29 09:45:25 +0200

    Filter second

--- src/Filter.js
+++ src/Filter.js
@@ -6,6 +6,7 @@ let employee = [{
     "name": "bob",
     "MealType" : "Lunch",
     "PriceRange": 30
+
 },{
     "employeeId": "2",
     "name": "steve",
@@ -28,26 +29,36 @@ let employee = [{
 }
 ];
 
+
+
 class Filter extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { checked: true };
+      }
+    
+      handleChange(event) {
+        this.setState( {checked: !this.state.checked} );
+      }
     render(){
         return(
                <div>
                 <h1>I Want to HAVE</h1>
                 <div class ="Check">
-                <input  type="checkbox" name="fruit" value="LUNCH" />Lunch
+                <input  type="checkbox" name="MealType" value="LUNCH" checked={this.state.value} onChange={this.handleChange} />Lunch
                 <br/>
-                <input  type="checkbox" name="fruit" value="DINNER" />Dinner
+                <input  type="checkbox" name="MealType" value="DINNER" checked={this.state.value} onChange={this.handleChange} />Dinner
                 <br/>
                 <h3>Price Range</h3>
-                <input  type="checkbox" name="fruit" value="15-25" />15-25
+                <input  type="checkbox" name="PriceRange" value="15-25" checked={this.state.value} onChange={this.handleChange} />15-25
                 <br/>
-                <input  type="checkbox" name="fruit" value="26-40" />26-40
+                <input  type="checkbox" name="PriceRange" value="26-40" checked={this.state.value} onChange={this.handleChange}/>26-40
                 <br/>
-                <input  type="checkbox" name="fruit" value="41-65" />41-65
+                <input  type="checkbox" name="PriceRange" value="41-65" checked={this.state.value} onChange={this.handleChange} />41-65
                 <br/>
-                <input  type="checkbox" name="fruit" value="66-100" />66-100
+                <input  type="checkbox" name="PriceRange" value="66-100" checked={this.state.value} onChange={this.handleChange}/>66-100
                 <br/>
-                <input  type="checkbox" name="fruit" value="Higher" />Higher
+                <input  type="checkbox" name="PriceRange" value="Higher" checked={this.state.value} onChange={this.handleChange} />Higher
                 <br/>
                 <br/>
                 <button>Submit</button>
