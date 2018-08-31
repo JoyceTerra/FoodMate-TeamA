ee60a1acc3adbf09e2723e4eff0ae982b2a2a591
JoyceTerra <joyterra@gmail.com>
2018-08-31 12:10:19 +0200

    Images added

--- src/reducers/image.js
+++ src/reducers/image.js
@@ -1,9 +1,18 @@
+import '../images/images.css'
 
-const IMG_1 = require('../images/asia2.jpg');
-const IMG_2 = require('../images/asia.jpg')
-const IMG_3 = require('../images/brunch.jpg')
-const IMG_4 = require('../images/pizza.jpg')
-const IMAGES = [IMG_1, IMG_2, IMG_3, IMG_4];
+
+
+const IMG_1 = require('../images/mariana-montes-de-oca-729502-unsplash.jpg');
+const IMG_2 = require('../images/ivan_torres_478678_u_7LfqR.jpg')
+const IMG_3 = require('../images/close_up_cooking_cui_yjJuA.jpg')
+const IMG_4 = require('../images/chopsticks-dinner-dish-33162.jpg')
+const IMG_5 = require('../images/cake-chocolate-chocolate-cake-132694.jpg')
+const IMG_6 = require('../images/bread_close_up_delic_fq3mV.jpg')
+const IMG_7 = require('../images/blueberry_bowl_break_OnUOG.jpg')
+const IMG_8 = require('../images/beverages_brunch_coc_2UdPY.jpg')
+const IMG_9 = require('../images/appetizer_cucumber_c_9l5CP.jpg')
+
+const IMAGES = [IMG_1, IMG_2, IMG_3, IMG_4, IMG_5, IMG_6, IMG_7,IMG_8, IMG_9];
 
 export default (state = IMAGES, action = {}) => {
     return state
